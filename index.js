08cd099d8c0fcb4cb79c45a116aac9342f591bd0
Kaan85 <koc.kaan.ibrahim@gmail.com>
2025-01-11 16:58:51 +0300

    express.js dersleri

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
-var http = require("http");
-var fs = require("fs");
-var server = http.createServer((req, res)=>{
-    if(req.url == "/"){
-        fs.readFile("index.html", (err, html) =>{
-            res.write(html);
-            res.end();
-        });
-        
-    }else if(req.url == "/products"){
-        fs.readFile("urunler.html", (err, html) =>{
-        res.write(html);
-        res.end();
-        });
-    }else{fs.readFile("404.html", (err, html) =>{
-        res.write(html);
-        res.end();
-        });
-    }
-    
+const express = require("express");
+const app = express();
+
+app.set("view engine","ejs");
+
+app.use(express.static('public'));
+app.use(express.static('node_modules'));
+const data = [
+    {id: 1, name:"İphone 14", price:30000,isActive: true,imageUrl:"1.jpeg"},
+    {id: 2, name:"İphone 15", price:40000,isActive: false,imageUrl:"2.jpeg"},
+    {id: 3, name:"İphone 16", price:50000,isActive: true,imageUrl:"3.jpeg"},
+];
+
+// routes
+app.use("/products/:id" ,function(req,res){
+    const urun = data.find(u => u.id == req.params.id);
+    res.render("products-details", urun);
 });
 
-server.listen(3000, () => {
-console.log('node.js server at port 3000');
+app.use("/products" ,function(req,res){
+    res.render("products",{urunler: data});
+});
+
+app.use("/" ,function(req,res ){
+    res.render("index");
+});
+
+app.listen(3000, ()=> {
+   console.log("listening on port 3000"); 
 });
