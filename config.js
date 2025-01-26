c0f7ce0e86d9343a018ae28ffb9851fc5833a188
Kaan85 <koc.kaan.ibrahim@gmail.com>
2025-01-26 23:50:18 +0300

    nodejsdeneme2

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+ db : {
+    host:"localhost",
+    user: "root",
+    password:"Antik.85",
+    database: "nodedb"
+}
+}
+
+module.exports = config;
--- index.js
+++ index.js
@@ -1,29 +1,13 @@
 const express = require("express");
 const app = express();
+const userRoutes = require("./routes/users");
 
 app.set("view engine","ejs");
 
 app.use(express.static('public'));
 app.use(express.static('node_modules'));
-const data = [
-    {id: 1, name:"İphone 14", price:30000,isActive: true,imageUrl:"1.jpeg"},
-    {id: 2, name:"İphone 15", price:40000,isActive: false,imageUrl:"2.jpeg"},
-    {id: 3, name:"İphone 16", price:50000,isActive: true,imageUrl:"3.jpeg"},
-];
 
-// routes
-app.use("/products/:id" ,function(req,res){
-    const urun = data.find(u => u.id == req.params.id);
-    res.render("products-details", urun);
-});
-
-app.use("/products" ,function(req,res){
-    res.render("products",{urunler: data});
-});
-
-app.use("/" ,function(req,res ){
-    res.render("index");
-});
+app.use(userRoutes);
 
 app.listen(3000, ()=> {
    console.log("listening on port 3000"); 
